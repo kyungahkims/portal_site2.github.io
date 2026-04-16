248a639630234ccf74c97448a89b9cc90b0aa5fe
경아 <sun4354@naver.com>
2026-04-16 12:47:42 +0900

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,3 +1,26 @@
+/* 스크롤 탑 버튼 */
+function onScroll() {
+	const scrollTop = window.scrollY || document.documentElement.scrollTop;
+	const btTop = document.querySelector(".bt_top");
+
+	if (scrollTop > 10) {
+		btTop.classList.add("show");
+	} else {
+		btTop.classList.remove("show");
+	}
+}
+
+function onTopClick() {
+	window.scrollTo({
+		top: 0,
+		behavior: "smooth"
+	});
+}
+
+window.addEventListener("scroll", onScroll);
+onScroll();
+document.querySelector(".bt_top").addEventListener("click", onTopClick);
+
 /* nav */
 const navLinks = document.querySelectorAll('.nav_wrap a');
 
